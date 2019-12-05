00431a05e1a9133289d6c5706846fdc488717a66
andrii.laptiuk <andrii.laptiuk@uklon.com.ua>
2019-12-05 16:42:25 +0200

    aspnetcore + nginx

---
+++ WebApp/WebApp/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.Run(async (context) =>
+            {
+                var result = await GetSqlNowAsync();
+                await context.Response.WriteAsync($"Hello World! It's {result}\n");
+            });
+        }
+
+        private async Task<dynamic> GetSqlNowAsync()
+        {
+            using (var connection = new NpgsqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                return (await connection.QueryAsync("select * from now();")).FirstOrDefault()?.now;
+            }
+        }
+    }
+}
